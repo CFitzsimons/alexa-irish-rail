4ddb39fd73b7e086eddaa9f93178c031ac507382
CFitzsimons <yulfy@outlook.com>
2017-09-15 00:08:37 +0100

    Rewrote to be more descriptive

--- src/index.js
+++ src/index.js
@@ -3,24 +3,18 @@ const handlers = require('./handlers/handlers');
 
 /* Basic config check */
 if (!process.env.appId) {
-  console.error('Please ensure you have set the appId environment variable.');
+  console.error('Please ensure you have set the appId environment variable. (env.appId)');
   process.exit(1);
 }
-
-/* Basic function to show Mocha tests, you can remove this */
-function whoIsNumberOne() {
-  return 'We are number 1';
+if (!process.env.tableId) {
+  console.error('Please ensure you supply the name of the table. (env.tableId)');
+  process.exit(1);
 }
-
-console.log(whoIsNumberOne());
-
-exports.whoIsNumberOne = whoIsNumberOne;
-/* End basic function section */
 
 /* Alexa Handlers are pulled from /handlers and given here */
 exports.handler = (event, context) => {
   const alexa = Alexa.handler(event, context);
   alexa.appId = process.env.appId;
-  alexa.registerHandlers(handlers);
+  alexa.registerHandlers(handlers.getIntents(event.session.user.userId));
   alexa.execute();
 };
