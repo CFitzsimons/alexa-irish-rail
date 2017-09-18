18cdc71dcc262a7312ddeb514d95219382e611e5
CFitzsimons <yulfy@outlook.com>
2017-09-18 20:02:47 +0100

    Added slot confirmation to setFavourite

--- src/intents/functions/setFavouriteIntent.js
+++ src/intents/functions/setFavouriteIntent.js
@@ -12,22 +12,26 @@ function isValidStation(station) {
 }
 
 module.exports = function setFavourite() {
-  if (!this.event.request
-    || !this.event.request.intent
-    || !this.event.request.intent.slots
+  if (!this.event.request.intent.slots
     || !this.event.request.intent.slots.StationName
     || !this.event.request.intent.slots.StationName.value) {
     this.emit(':delegate');
     return;
   }
   const slotValue = this.event.request.intent.slots.StationName.value;
+  const slotStatus = this.event.request.intent.slots.StationName.confirmationStatus;
   if (!isValidStation(slotValue)) {
-    this.emit(':delegate');
+    this.emit(':elicitSlot', 'StationName', 'That doesn\'t appear to be a valid station, could you try again?', 'What station would you like to favourite?');
     return;
   }
-
-  const db = new DatabaseProxy(process.env.tableId, this.event.session.user.userId);
-  db.store(slotValue).then(() => {
-    this.emit(':tell', `I saved ${slotValue} as your favourite!`, `I saved ${slotValue} as your favourite!`);
-  });
+  if (slotStatus === 'NONE') {
+    this.emit(':confirmSlot', 'StationName', `Are you sure you want to set ${slotValue} as your favourite?`, `Is ${slotValue} correct?`);
+  } else if (slotStatus === 'CONFIRMED') {
+    const db = new DatabaseProxy(process.env.tableId, this.event.session.user.userId);
+    db.store(slotValue).then(() => {
+      this.emit(':tell', `I saved ${slotValue} as your favourite!`, `I saved ${slotValue} as your favourite!`);
+    });
+  } else {
+    this.emit(':elicitSlot', 'StationName', 'Oh, OK.  What station would you like to favourite then?', 'What station would you like to favourite?');
+  }
 };
